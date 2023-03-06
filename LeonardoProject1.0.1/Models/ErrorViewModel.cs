3b39dc8fdde1e00ab766463e8502b04e0e9e72d6
João Gabriel <85807914+JoaoGabriel-JG@users.noreply.github.com>
2023-03-06 14:06:48 -0300

    att

---
+++ LeonardoProject1.0.1/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace LeonardoProject1._0._1.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
